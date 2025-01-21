e36dcff6d54f7dc83cd985ee59281fe756e7d9c1
Bruno Carletto Adamczyk <150372999+BruDu1545@users.noreply.github.com>
2025-01-21 12:11:22 -0300

    Add files via upload

---
+++ fist_project/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'app_controller.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Switch(
+          value: AppController.instance.isDarkTheme, 
+          onChanged: (value){
+            setState(() {
+              AppController.instance.changeTheme();
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
